c1de1e1c9a8617f6418cbabb52c30a3efa9cd8da
beetee17 <brandon.thio@yahoo.com.sg>
2021-07-01 19:02:04 +0800

    Initial Commit

---
+++ Word Bomb/Word_BombApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_BombApp.swift
+//  Word Bomb
+//
+//  Created by Brandon Thio on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Word_BombApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

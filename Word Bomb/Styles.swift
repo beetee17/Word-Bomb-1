514224396db5a1420a16e561f1cd17c0de201fc2
beetee17 <brandon.thio@yahoo.com.sg>
2021-07-03 13:12:20 +0800

    fixed hitbox of buttons and add animation when pressed

--- Word Bomb/Styles.swift
+++ Word Bomb/Styles.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct MainButtonStyle: ButtonStyle {
+    let width = UIScreen.main.bounds.width/2
+    let height = UIScreen.main.bounds.height*0.07
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .font(Font.title.bold())
-            .frame(width:UIScreen.main.bounds.width/2,
-                   height:UIScreen.main.bounds.height*0.05)
+            .frame(width: width, height: height)
             .foregroundColor(Color.black)
-            .padding()
+            .contentShape(RoundedRectangle(cornerRadius: 20))
             .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.black, lineWidth: 5))
+            .scaleEffect(configuration.isPressed ? 1.2 : 1.0)
+            
     }
 }
 
